e9e9c69365fb8ce1cf21d6470a7fdc827aea7f08
Checco <f.laurendi.2696@vallauri.edu>
2025-02-19 08:52:24 +0100

    setup del backend

---
+++ backend/server.ts
@@ -0,0 +1,29 @@
+import express, {Request, Response} from 'express';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config(); // serve per mettere le variabili di sistema
+
+const MONGO_URI = process.env.MONGO_URI || "";
+const PORT = 4000;
+const DB_NAME = "5E";
+const COLLECTION_NAME = "unicorns";
+
+let ex = express();
+const client = new MongoClient(MONGO_URI);
+
+async function connectDB() {
+
+    try
+    {
+        await client.connect();
+        console.log("Connesso");
+    }
+    catch(error){
+        console.log("Errore", error);
+    }
+}
+
+ex.listen(PORT, () => {
+    console.log(`Server in ascolto su: http://localhost:${PORT}`);
+});
